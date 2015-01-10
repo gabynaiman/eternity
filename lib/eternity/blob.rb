782a27d0ff53e1af916d7d21170325f1cf729924
Gabriel Naiman <gabynaiman@gmail.com>
2015-01-10 02:42:49 -0300

    Refactor blob

--- lib/eternity/blob.rb
+++ lib/eternity/blob.rb
@@ -8,83 +8,87 @@ module Eternity
       @sha1 = sha1
     end
 
-    def value
+    def data
       sha1 ? Blob.read(type, sha1) : {}
     end
+
+    class << self
   
-    def self.write(type, data)
-      serialization = serialize data
-      sha1 = digest serialization
+      def write(type, data)
+        serialization = serialize data
+        sha1 = digest serialization
 
-      write_redis type, sha1, serialization
-      Thread.new { write_file type, sha1, serialization }
+        write_redis type, sha1, serialization
+        Thread.new { write_file type, sha1, serialization }
 
-      sha1
-    end
-
-    def self.read(type, sha1)
-      deserialize read_redis(type, sha1) || read_file(type, sha1)
-    end
-
-    def self.digest(string)
-      Digest::SHA1.hexdigest string
-    end
-
-    def self.sanitize(data)
-      data.each { |k,v| data[k] = v.strftime TIME_FORMAT if v.is_a? Time }
-      data
-    end
-
-    def self.serialize(data)
-      MessagePack.pack sanitize(data)
-    end
-
-    def self.deserialize(string)
-      MessagePack.unpack string
-    end
-
-    def self.clear_cache
-      Eternity.redis.call('KEYS', Eternity.keyspace[:blob]['*']).each_slice(1000) do |keys|
-        Eternity.redis.call 'DEL', *keys
+        sha1
       end
-    end
 
-    def self.count
-      Eternity.redis.call('KEYS', Eternity.keyspace[:blob]['*']).count
-    end
-
-    private
-
-    def self.write_redis(type, sha1, serialization)
-      Eternity.redis.call 'SET', Eternity.keyspace[:blob][type][sha1], serialization, 
-                          'EX', Eternity.blob_cache_expiration
-    end
-
-    def self.read_redis(type, sha1)
-      Eternity.redis.call 'GET', Eternity.keyspace[:blob][type][sha1]
-    end
-
-    def self.write_file(type, sha1, serialization)
-      filename = file_for type, sha1
-      if !File.exists? filename
-        dirname = File.dirname filename
-        FileUtils.mkpath dirname unless Dir.exists? dirname
-        File.write filename, serialization
+      def read(type, sha1)
+        deserialize read_redis(type, sha1) || read_file(type, sha1)
       end
-    end
 
-    def self.read_file(type, sha1)
-      serialization = IO.read file_for(type, sha1)
-      Thread.new { write_redis type, sha1, serialization }
-      serialization
+      def digest(string)
+        Digest::SHA1.hexdigest string
+      end
 
-    rescue Errno::ENOENT
-      raise "Blob not found. #{type.capitalize}: #{sha1}"
-    end
+      def sanitize(data)
+        data.each { |k,v| data[k] = v.strftime TIME_FORMAT if v.is_a? Time }
+        data
+      end
 
-    def self.file_for(type, sha1)
-      File.join Eternity.data_path, 'blob', type.to_s, sha1[0..1], sha1[2..-1]
+      def serialize(data)
+        MessagePack.pack sanitize(data)
+      end
+
+      def deserialize(string)
+        MessagePack.unpack string
+      end
+
+      def clear_cache
+        Eternity.redis.call('KEYS', Eternity.keyspace[:blob]['*']).each_slice(1000) do |keys|
+          Eternity.redis.call 'DEL', *keys
+        end
+      end
+
+      def count
+        Eternity.redis.call('KEYS', Eternity.keyspace[:blob]['*']).count
+      end
+
+      private
+
+      def write_redis(type, sha1, serialization)
+        Eternity.redis.call 'SET', Eternity.keyspace[:blob][type][sha1], serialization, 
+                            'EX', Eternity.blob_cache_expiration
+      end
+
+      def read_redis(type, sha1)
+        Eternity.redis.call 'GET', Eternity.keyspace[:blob][type][sha1]
+      end
+
+      def write_file(type, sha1, serialization)
+        filename = file_for type, sha1
+        if !File.exists? filename
+          dirname = File.dirname filename
+          FileUtils.mkpath dirname unless Dir.exists? dirname
+          File.write filename, serialization
+        end
+      end
+
+      def read_file(type, sha1)
+        serialization = IO.read file_for(type, sha1)
+        Thread.new { write_redis type, sha1, serialization }
+        serialization
+
+      rescue Errno::ENOENT
+        raise "Blob not found. #{type.capitalize}: #{sha1}"
+      end
+
+      def file_for(type, sha1)
+        File.join Eternity.data_path, 'blob', type.to_s, sha1[0..1], sha1[2..-1]
+      end
+
     end
-  
+    
   end
 end
