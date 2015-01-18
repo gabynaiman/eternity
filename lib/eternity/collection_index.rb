0d352df446751fe2d5cc73281fc62a35a1180bac
Gabriel Naiman <gabynaiman@gmail.com>
2015-01-18 16:37:05 -0300

    Rename hash to index

--- lib/eternity/collection_index.rb
+++ lib/eternity/collection_index.rb
@@ -2,39 +2,43 @@ module Eternity
   class CollectionIndex
 
     extend Forwardable
-    def_delegators :hash, :to_h, :to_primitive, :empty?, :dump, :restore, :destroy
+    def_delegators :index, :to_h, :to_primitive, :empty?, :dump, :restore, :destroy
 
     def initialize(options)
-      @hash = Restruct::Hash.new options
+      @index = Restruct::Hash.new options
     end
 
     def collection_name
-      hash.key.sections.last
+      index.key.sections.last
     end
 
     def [](id)
-      return nil unless hash.key? id
-      Blob.new :data, hash[id]
+      return nil unless index.key? id
+      Blob.new :data, index[id]
     end
 
     def insert(id, data)
-      raise "#{collection_name.capitalize} #{id} already exists" if hash.key? id
-      hash[id] = Blob.write :data, data
+      raise "#{collection_name.capitalize} #{id} already exists" if index.key? id
+      index[id] = Blob.write :data, data
     end
 
     def update(id, data)
-      raise "#{collection_name.capitalize} #{id} not found" unless hash.key? id
-      hash[id] = Blob.write :data, data
+      raise "#{collection_name.capitalize} #{id} not found" unless index.key? id
+      index[id] = Blob.write :data, data
     end
 
     def delete(id)
-      raise "#{collection_name.capitalize} #{id} not found" unless hash.key? id
-      hash.delete id
+      raise "#{collection_name.capitalize} #{id} not found" unless index.key? id
+      index.delete id
+    end
+
+    def ids
+      index.keys
     end
 
     private
 
-    attr_reader :hash
+    attr_reader :index
 
   end
 end
