5f1aa9e64e308985f28b59babea8722955cee704
Damian Aberbuj <daberbuj@keepcon.com>
2015-12-22 17:29:00 -0300

    Refactor in delta merge

--- lib/eternity/delta.rb
+++ lib/eternity/delta.rb
@@ -18,12 +18,12 @@ module Eternity
         union(deltas).each_with_object({}) do |(collection, elements), hash|
           hash[collection] = {}
           elements.each do |id, changes|
-            base_data = base_index[collection].include?(id) ? base_index[collection][id].data : {}
             changes.each do |change|
               current_change = change
               if hash[collection][id]
                 current_change = TrackFlatter.flatten [hash[collection][id], change]
                 if current_change && [INSERT, UPDATE].include?(current_change['action'])
+                  base_data = base_index[collection].include?(id) ? base_index[collection][id].data : {}
                   current_change['data'] = ConflictResolver.resolve hash[collection][id]['data'] || base_data,
                                                                     change['data'],
                                                                     base_data
